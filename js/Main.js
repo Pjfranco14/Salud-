f12c46a5a59dc6023a3065143a9aaebb49e612bc
Emmanuel Fuentes <jefuentes@scl.edu.gt>
2021-09-27 09:53:36 -0600

    Colaboración del profe

--- js/Main.js
+++ js/Main.js
@@ -4,18 +4,16 @@ var formulario2 = document.querySelector("#inputFormulario");
 var btnsiguiente = document.querySelector("#btn-siguiente");
 
 
-function CambiarFormulario(){
-    var formulario1 = document.querySelector("#formulario-nombre");
-    formulario1.classList.add('activarCaja');
-}
-CambiarFormulario();
-    var btnsiguiente = document.querySelector("#btn-siguiente");
-    btnsiguiente.addEventListener('click', function(){
-    
+function CambiarFormulario(e){
+    e.preventDefault();
+
     var formulario2 = document.querySelector("#inputFormulario");
-    var formulario1 = document.querySelector("#formulario-nombre");
+    var formulario1 = document.querySelector("#formulario-nommbre");
 
     formulario2.classList.add('activarCaja');
-    formulario1.classList.remove('activarCaja');
-});
+    formulario1.classList.add('desactivarCaja');
+};
 
+/* ACTIVAR BOTON */
+var btnsiguiente = document.querySelector("#btn-siguiente");
+btnsiguiente.addEventListener('click', CambiarFormulario);
